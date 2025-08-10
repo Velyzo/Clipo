95f678b0dce64353240a7807b10fea2c7fa5f571
wfxey <devinoldenburg16@gmail.com>
2025-08-10 22:09:15 +0200

    Initial Commit

---
+++ Clipo/ClipoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipoApp.swift
+//  Clipo
+//
+//  Created by Devin on 10.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ClipoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipoTests/ClipoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipoTests.swift
+//  ClipoTests
+//
+//  Created by Devin on 10.08.25.
+//
+
+import Testing
+@testable import Clipo
+
+struct ClipoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
